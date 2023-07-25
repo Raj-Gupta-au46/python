327549a8b71cbbb68bf3e85e40369327d5fb92a6
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-07-26 01:17:09 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,18 @@
+# print("Hello World")
+# print("Hi I am back")
+
+a = 1154184454516
+print(a)
+b="Raj"
+print(b)
+d=b
+print(d)
+a1="124"
+# datatypes --
+# print(a+b) // gives error because of different datatypes 
+print(a1+b)
+
+# for finding type of any variable we use type example
+z="hi"
+print("the type of a is \n ", type(z))
+print("the type of a is \n ", type(a))
